ba0e90e57b3d0f6e46a1669d85a3cb0032abd6ce
mariemhaj531 <mariemhajji28531@gmail.com>
2025-04-14 13:44:10 +0200

    Design App

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,36 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-// import logo from '../assets/logo.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import logo from '../assets/logo.png';
+
 const Navbar = () => {
   return (
-    <nav className='border-b-4 border-green-700 text-center fixed top-0 bg-green-600 font-bold w-full text-lg text-white'>
-       {/* <Link to="/">
-          <img src={logo} alt="Logo" className="" />
-        </Link>  */}
-            <ul>
-        <li className='inline-block py-4'>
-            <Link to="/" className='pl-6 pr-8'>
+    <nav className="fixed top-0 w-full bg-blue-900 shadow-md border-b-4 border-blue-700 z-50">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex justify-between h-16 items-center">
+        {/* Logo ou Nom */}
+        <div className="flex-shrink-0 text-white text-2xl font-extrabold tracking-wide">
+          <Link to="/">MaymaBlog</Link>
+        </div>
+        {/* Liens de navigation */}
+        <div className="hidden md:flex space-x-8 text-white font-semibold text-lg">
+          <Link
+            to="/"
+            className="hover:text-blue-300 transition duration-200 ease-in-out"
+          >
             Home
-            </Link>
-        </li>
-        <li className='inline-block py-4'>
-            <Link to="/about" className='pl-6 pr-8'>
+          </Link>
+          <Link
+            to="/about"
+            className="hover:text-blue-300 transition duration-200 ease-in-out"
+          >
             About
-            </Link>
-        </li>
-        <li className='inline-block py-4'>
-            <Link to="/articles-list" className='pl-6 pr-8'>
+          </Link>
+          <Link
+            to="/articles-list"
+            className="hover:text-blue-300 transition duration-200 ease-in-out"
+          >
             Blogs
-            </Link>
-        </li>
-        <li className='inline-block py-4'>
-            <Link to="/contact" className='pl-6 pr-8'>
+          </Link>
+          <Link
+            to="/contact"
+            className="hover:text-blue-300 transition duration-200 ease-in-out"
+          >
             Contact
-            </Link>
-        </li>
-     </ul>
-    </nav>
-  )
-}
+          </Link>
+        </div>
+      </div>
+    </div>
+  </nav>
+  
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/pages/ArticlesList.js
+++ client/src/pages/ArticlesList.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from 'react';
+import articleContent from "./Article-content";
+import { Link } from 'react-router-dom';
 
 const ArticlesList = () => {
   return (
-    <div>
-    <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
-    Blogs
-    </h1>
+    <div className="px-4 py-8">
+      <h1 className="text-3xl sm:text-4xl font-bold mb-6 text-gray-900">Articles</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {articleContent.map((article, index) => (
+          <Link
+            key={index}
+            to={`/article/${article.name}`}
+            className="bg-white rounded-xl shadow hover:shadow-md transition duration-300 overflow-hidden"
+          >
+            <img
+              className="w-full h-48 object-cover"
+              src={article.thumbnail}
+              alt={article.title}
+            />
+            <div className="p-4">
+              <h3 className="text-base font-semibold text-gray-800">{article.title}</h3>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ArticlesList
+export default ArticlesList;
